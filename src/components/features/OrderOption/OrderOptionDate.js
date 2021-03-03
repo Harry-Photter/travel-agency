2e70c5ffcbca8a97d3183904435af18b44dbccd1
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-03-03 19:06:50 +0100

    Add setOptionValue to OrderOptionText&...Date

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 
-const OrderOptionDate = () => {
+const OrderOptionDate = ( props ) => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
     <div className={styles.text}>
-      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+      <DatePicker 
+        selected={startDate} 
+        onChange={date => {
+          setStartDate(date);
+          props.setOptionValue(date);
+        }} />
     </div>
   );
 };
 
-// OrderOptionDate.propTypes = {
-
-// };
+OrderOptionDate.propTypes = {
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionDate;
--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -1,19 +1,20 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 
-const OrderOptionText = () => {
+const OrderOptionText = ( props ) => {
   return (
     <div className={styles.text}>
       <input
         type="text"
+        onChange={event => props.setOptionValue(event.currentTarget.value)}
       />
     </div>
   );
 };
 
-// OrderOptionText.propTypes = {
-
-// };
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionText;
